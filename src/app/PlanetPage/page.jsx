22f5bb9fb7f21b382622920b9eec000568a551a5
Savva <imperiya66.ru@gmail.com>
2025-04-17 22:16:40 +0500

    IvanSavva

--- src/app/PlanetPage/page.jsx
+++ src/app/PlanetPage/page.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import stules from './styles.module.css'
-
-const page = () => {
+import Image from "next/image";
+import styles from "./styles.module.css";
+// import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { MatrixRain } from "../../components/MatrixRain/MatrixRain";
+export default function Home() {
   return (
-    <div className={stules.wrapper}>
-    <div className={stules.text}>Matrix</div>
-    </div>
-  )
+    <>
+      <nav className={styles.wrapper}>
+        <Link href={"/education"} className={styles.text}>
+          Учение.
+        </Link>
+        <Link href={"/price"} className={styles.text}>
+          Организации.
+        </Link>
+        <Link href={"/keys"} className={styles.text}>
+          Книги.
+        </Link>
+        <Link href={"/products"} className={styles.text}>
+          Магазины.
+        </Link>
+        <Link href={"/test"} className={styles.text}>
+          Тесты.
+        </Link>
+        <MatrixRain></MatrixRain>
+      </nav>
+    </>
+  );
 }
-
-export default page
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,18 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-// import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import stules from "./page.module.css";
 import Link from "next/link";
-import { MatrixRain } from "../components/MatrixRain/MatrixRain";
-export default function Home() {
+
+import { MatrixRain } from "./../components/MatrixRain/MatrixRain"
+
+const page = () => {
   return (
-    <>
-      <nav className={styles.wrapper}>
-        <Link href={"/education"} className={styles.text}>
-          Учение.
-        </Link>
-        <Link href={"/price"} className={styles.text}>
-          Организации.
-        </Link>
-        <Link href={"/keys"} className={styles.text}>
-          Книги.
-        </Link>
-        <Link href={"/products"} className={styles.text}>
-          Магазины.
-        </Link>
-        <Link href={"/test"} className={styles.text}>
-          Тесты.
-        </Link>
-        <MatrixRain></MatrixRain>
-      </nav>
-    </>
+    <div>
+    <div className={stules.wrapper}>
+      <Link href="/PlanetPage" className={stules.text}>Matrix</Link>
+      </div>      
+      <MatrixRain></MatrixRain>
+    </div>
   );
-}
+};
+
+export default page;
