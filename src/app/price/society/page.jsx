4f24ac60362ddcf112ea265656331900df88155d
Savva <imperiya66.ru@gmail.com>
2025-05-08 14:43:54 +0500

    Savva

---
+++ src/app/price/society/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+import React from "react";
+import { MatrixRain } from "@/components/MatrixRain/MatrixRain";
+
+const page = () => {
+  return (
+      <div className={styles.wrapper}>
+          <div className={styles.text}>
+            <div>Выпущено тап 34.</div>
+            <div>Стоимость тапы 20 рублей.</div>
+            <div>Бюджет Акционерного общества 680 рублей.</div>
+            <Link href="/price">Вернуться.</Link>
+          </div>
+          <MatrixRain></MatrixRain>
+      </div>
+  );
+};
+
+export default page;
